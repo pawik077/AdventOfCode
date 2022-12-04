27aba024c870088d399df49ca9c8dac82f1a9627
pawik077 <pawik.r.s.077@gmail.com>
2022-12-04 13:24:42 +0100

    Day four

---
+++ 4/1.py
@@ -0,0 +1,10 @@
+with open('input', 'r') as f:
+	data = f.read().splitlines()
+subsets = 0
+for line in data:
+	first, second = line.split(',')
+	first = range(int(first.split('-')[0]), int(first.split('-')[1]) + 1)
+	second = range(int(second.split('-')[0]), int(second.split('-')[1]) + 1)
+	if(all(x in second for x in first) or all(x in first for x in second)):
+		subsets += 1
+print(subsets)
