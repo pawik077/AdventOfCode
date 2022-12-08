25fbcf1d019928b6f54859b88264a71e7fe8b508
pawik077 <pawik.r.s.077@gmail.com>
2022-12-08 16:37:20 +0100

    Improved day three

--- 2022/3/2.py
+++ 2022/3/2.py
@@ -1,57 +1,6 @@
-d = {
-	'a': 1,
-	'b': 2,
-	'c': 3,
-	'd': 4,
-	'e': 5,
-	'f': 6,
-	'g': 7,
-	'h': 8,
-	'i': 9,
-	'j': 10,
-	'k': 11,
-	'l': 12,
-	'm': 13,
-	'n': 14,
-	'o': 15,
-	'p': 16,
-	'q': 17,
-	'r': 18,
-	's': 19,
-	't': 20,
-	'u': 21,
-	'v': 22,
-	'w': 23,
-	'x': 24,
-	'y': 25,
-	'z': 26,
-	'A': 27,
-	'B': 28,
-	'C': 29,
-	'D': 30,
-	'E': 31,
-	'F': 32,
-	'G': 33,
-	'H': 34,
-	'I': 35,
-	'J': 36,
-	'K': 37,
-	'L': 38,
-	'M': 39,
-	'N': 40,
-	'O': 41,
-	'P': 42,
-	'Q': 43,
-	'R': 44,
-	'S': 45,
-	'T': 46,
-	'U': 47,
-	'V': 48,
-	'W': 49,
-	'X': 50,
-	'Y': 51,
-	'Z': 52
-}
+def get_priority(char):
+	return ord(char) - 96 if char.islower() else ord(char) - 38
+
 with open('input', 'r') as f:
 	data = f.read().splitlines()
 priorities = 0
@@ -64,6 +13,7 @@ for block in split:
 	seen = []
 	for char in block[0]:
 		if char in block[1] and char in block[2] and char not in seen:
-			priorities += d[char]
+			priorities += get_priority(char)
 			seen.append(char)
+
 print(priorities)
