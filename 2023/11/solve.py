598390cb9f94701311ab2c4502ba9fb469c93d97
pawik077 <pawik.r.s.077@gmail.com>
2024-01-04 14:59:59 +0100

    Day eleven complete

--- 2023/11/solve.py
+++ 2023/11/solve.py
@@ -1,6 +1,7 @@
 import sys
 
 def part1(data):
+    data = data.copy()
     i = 0
     while i < len(data):
         if not '#' in data[i]:
@@ -27,7 +28,28 @@ def part1(data):
     return sum(paths)
 
 def part2(data):
-    return None
+    empty_rows = []
+    for i, row in enumerate(data):
+        if not '#' in row:
+            empty_rows.append(i)
+    empty_cols = []
+    data = list(map(list, zip(*data)))
+    for i, col in enumerate(data):
+        if not '#' in col:
+            empty_cols.append(i)
+    data = list(map(list, zip(*data)))
+    galaxies = {}
+    for y, row in enumerate(data):
+        for x, cell in enumerate(row):
+            if cell == '#':
+                real_y = y + len([row for row in empty_rows if row < y]) * 999999
+                real_x = x + len([col for col in empty_cols if col < x]) * 999999
+                galaxies[len(galaxies)] = (real_x, real_y)
+    paths = []
+    for i in range(len(galaxies)):
+        for j in range(i+1, len(galaxies)):
+            paths.append(abs(galaxies[i][0] - galaxies[j][0]) + abs(galaxies[i][1] - galaxies[j][1]))
+    return sum(paths)
 
 if __name__ == '__main__':
     filename = 'input' if '-t' not in sys.argv else 'test'
